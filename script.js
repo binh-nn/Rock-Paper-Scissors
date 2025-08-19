8a7c998d9ac60dcd865dd982ba2759558be4128a
binh-nn <141991082+binh-nn@users.noreply.github.com>
2025-08-18 21:46:58 -0700

    Added getHumanChoice function

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function getComputerChoice () {
     // Generates number that will correlate to the choice
     let choice = Math.floor(Math.random() * 3);
-    
+
+    // Condition to check whether to chose Rock Paper or Scissors 
     if (choice === 0) {
         return "rock";
     } else if (choice === 1) {
@@ -11,4 +12,10 @@ function getComputerChoice () {
     }
 }
 
+function getHumanChoice() {
+    let choice = prompt("Please type rock, paper, or scissors.");
+    return choice;
+}
+
 console.log(getComputerChoice());
+console.log(getHumanChoice());
